5d5a2a94d3deb6516371a31fe7f90b4ceb80161f
Jennifer Thompson <thompson.jennifer@gmail.com>
2017-06-08 17:02:17 -0500

    Add data management for screening and exclusions page.

--- datamgmt_progress.R
+++ datamgmt_progress.R
@@ -3,6 +3,7 @@
 ################################################################################
 
 library(RCurl)
+library(tidyverse)
 
 ## -- Import each data set from REDCap (exclusions, in-hospital, follow-up) ----
 ## All tokens are stored in .Renviron
@@ -41,3 +42,69 @@ exc_df <- import_df("MOSAIC_EXC_TOKEN")
 ## Remove test patients from each database
 inhosp_df <- inhosp_df[grep("test", tolower(inhosp_df$id), invert = TRUE),]
 exc_df <- exc_df[grep("test", tolower(exc_df$exc_id), invert = TRUE),]
+
+################################################################################
+## Screening and Exclusions
+################################################################################
+
+## -- Barchart for screening and enrollment by month ---------------------------
+## We want to plot the number of patients screened, approached, and enrolled by
+## month. Need a list of all unique IDs (exclusions + enrolled).
+
+## Screened: Everyone recorded
+## Approached: Enrolled + refusals
+## Refused: exclusion #14 checked
+##      (Inability to obtain informed consent: Patient and/or surrogate refusal)
+## Enrolled: Included in in-hospital database
+
+exc_combine <- exc_df %>%
+  separate(exc_date, into = c("year", "month", "day"), sep = "-") %>%
+  mutate(myear = paste(month, year, sep = "-"),
+         myear_char = paste(month.abb[as.numeric(month)], year),
+         Screened = TRUE,
+         Approached = !is.na(exc_rsn_14),
+         Refused = !is.na(exc_rsn_14),
+         Enrolled = FALSE) %>%
+  rename(id = exc_id) %>%
+  dplyr::select(id, year, month, Screened, Approached, Refused, Enrolled)
+
+inhosp_combine <- inhosp_df %>%
+  filter(redcap_event_name == 'Enrollment /Trial Day 1') %>%
+  separate(enroll_dttm, into = c("year", "month", "day", "time"), sep = "-| ") %>%
+  mutate(Screened = TRUE,
+         Approached = TRUE,
+         Refused = FALSE,
+         Enrolled = TRUE) %>%
+  dplyr::select(id, year, month, Screened, Approached, Refused, Enrolled)
+
+screening_combine <- bind_rows(exc_combine, inhosp_combine) %>%
+  mutate(mabb = month.abb[as.numeric(month)],
+         myear = paste(month, year, sep = "-"),
+         myear_char = ifelse(mabb == "Mar", paste(mabb, year), mabb))
+
+screening_summary <- screening_combine %>%
+  group_by(myear, myear_char) %>%
+  summarise_each(funs(sum), Screened, Approached, Refused, Enrolled)
+
+## How many patients have been enrolled so far? What is our enrollment goal?
+n_screened <- sum(screening_combine$Screened)
+pct_approached <- mean(screening_combine$Approached)
+pct_excluded <- 1 - pct_approached
+pct_refused <- mean(subset(screening_combine, Approached)$Refused)
+n_enrolled <- sum(screening_combine$Enrolled)
+pct_enrolled <- mean(subset(screening_combine, Approached)$Enrolled)
+n_goal <- 500
+
+## -- Line chart for exclusion percentages over time ---------------------------
+exc_df <- exc_df %>%
+  separate(exc_date, into = c("year", "month", "day"), sep = "-") %>%
+  mutate(mabb = month.abb[as.numeric(month)],
+         myear = paste(month, year, sep = "-"),
+         myear_char = ifelse(mabb == "Mar", paste(mabb, year), mabb))
+
+calc_pct_notmissing <- function(x){ mean(!is.na(x)) * 100 }
+
+exc_over_time <- exc_df %>%
+  group_by(myear, myear_char) %>%
+  summarise_each(funs(calc_pct_notmissing), exc_rsn_1:exc_rsn_99) %>%
+  gather(key = Reason, value = Percent, exc_rsn_1:exc_rsn_99)
