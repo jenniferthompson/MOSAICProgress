b75faf399f4cedbac824c3006b9c05eef5b5de97
Jennifer Thompson <thompson.jennifer@gmail.com>
2017-06-13 13:23:46 -0500

    First draft of screening and enrollment page done.

--- datamgmt_progress.R
+++ datamgmt_progress.R
@@ -29,10 +29,10 @@ get_csv <- function(pF){
 import_df <- function(rctoken){
   tmp_pF <- get_pF(rctoken)
   tmp_csv <- get_csv(tmp_pF)
-  
+
   ## REDCap loves to use so many underscores; one per instance seems like plenty
   names(tmp_csv) <- gsub("_+", "_", names(tmp_csv))
-  
+
   tmp_csv
 }
 
@@ -93,18 +93,74 @@ pct_excluded <- 1 - pct_approached
 pct_refused <- mean(subset(screening_combine, Approached)$Refused)
 n_enrolled <- sum(screening_combine$Enrolled)
 pct_enrolled <- mean(subset(screening_combine, Approached)$Enrolled)
-n_goal <- 500
+n_goal <- 312
 
 ## -- Line chart for exclusion percentages over time ---------------------------
-exc_df <- exc_df %>%
+## Create long-format data set of all exclusions, one row each
+exc_df_long <- exc_df %>%
+  gather(key = exc_reason, value = was_excluded, exc_rsn_1:exc_rsn_99) %>%
   separate(exc_date, into = c("year", "month", "day"), sep = "-") %>%
-  mutate(mabb = month.abb[as.numeric(month)],
+  mutate(was_excluded = !is.na(was_excluded),
+         mabb = month.abb[as.numeric(month)],
          myear = paste(month, year, sep = "-"),
-         myear_char = ifelse(mabb == "Mar", paste(mabb, year), mabb))
+         myear_char = ifelse(mabb == "Mar", paste(mabb, year), mabb),
+         Reason = ifelse(exc_reason == "exc_rsn_1", "Rapidly resolving organ failure",
+                  ifelse(exc_reason == "exc_rsn_2", ">5 hospital days in last 30",
+                  ifelse(exc_reason == "exc_rsn_3", "Inability to live independently",
+                  ifelse(exc_reason == "exc_rsn_4", "Severe neurologic injury",
+                  ifelse(exc_reason == "exc_rsn_5", "BMI > 50",
+                  ifelse(exc_reason == "exc_rsn_6", "Substance abuse, etc",
+                  ifelse(exc_reason == "exc_rsn_7", "Blind, deaf, English",
+                  ifelse(exc_reason == "exc_rsn_8", "Death within 24h/hospice",
+                  ifelse(exc_reason == "exc_rsn_9", "Prisoner",
+                  ifelse(exc_reason == "exc_rsn_10", "Lives >150 miles from VUMC",
+                  ifelse(exc_reason == "exc_rsn_11", "Homeless",
+                  ifelse(exc_reason == "exc_rsn_12", "Study with no co-enrollment",
+                  ifelse(exc_reason == "exc_rsn_13", "Attending refusal",
+                  ifelse(exc_reason == "exc_rsn_14", "Patient/surrogate refusal",
+                  ifelse(exc_reason == "exc_rsn_15", "No surrogate within 72h",
+                  ifelse(exc_reason == "exc_rsn_99", "Other",
+                         NA))))))))))))))))) %>%
+  filter(was_excluded)
 
-calc_pct_notmissing <- function(x){ mean(!is.na(x)) * 100 }
+## Data set for exclusions over time: Proportion of each exclusion each month
+## How many exclusions total per month?
+exc_per_month <- exc_df_long %>%
+  dplyr::select(exc_id, myear, was_excluded) %>%
+  unique() %>%
+  group_by(myear) %>%
+  summarise(n_all_exclusions = sum(was_excluded))
 
-exc_over_time <- exc_df %>%
-  group_by(myear, myear_char) %>%
-  summarise_each(funs(calc_pct_notmissing), exc_rsn_1:exc_rsn_99) %>%
-  gather(key = Reason, value = Percent, exc_rsn_1:exc_rsn_99)
+exc_over_time <- exc_df_long %>%
+  group_by(myear, myear_char, Reason) %>%
+  summarise(n_this_exclusion = sum(was_excluded)) %>%
+  left_join(exc_per_month, by = "myear") %>%
+  mutate(Percent = round((n_this_exclusion / n_all_exclusions)*100))
+
+## -- Treemap for cumulative exclusions ----------------------------------------
+exc_cumul <- exc_df_long %>%
+  group_by(Reason) %>%
+  summarise(n_reason = n()) %>%
+  mutate(n_patients_exc = nrow(exc_df),
+         reason_type = case_when(
+           .$Reason %in% c(
+             "Inability to live independently",
+             "Homeless",
+             "BMI > 50",
+             "Blind, deaf, English",
+             "Substance abuse, etc"
+            ) ~ "Patient characteristics",
+           .$Reason %in% c(
+             ">5 hospital days in last 30",
+             "Death within 24h/hospice",
+             "Rapidly resolving organ failure",
+             "Severe neurologic injury"
+           ) ~ "Medical exclusions",
+           .$Reason %in% c("Lives >150 miles from VUMC") ~ "Geography",
+           .$Reason %in% c(
+             "Attending refusal",
+             "No surrogate within 72h",
+             "Patient/surrogate refusal"
+           ) ~ "Informed consent",
+           TRUE ~ "Other"
+         ))
